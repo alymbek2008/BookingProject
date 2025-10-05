90008ebd5ad041aec772abff8e13f38157e24c58
алымбек2008 <qwerttqwerr23@gmail.com>
2025-10-05 14:17:52 +0600

    добавил два пайтон файла

---
+++ MKLEFOer.py
@@ -0,0 +1,11 @@
+fd vjdf vjv j vj vars(r j j vv
+ jkbkj   krjgbnen
+vkjnkjenk
+rnegnioeng
+gnoieoen errnorrnelnegr
+rginrognrk
+errnorekr r lkgnerngr
+mknnergjnerkjre reversedrk nklgkjgrr reversedrl
+mglnlgrngnkeg ekngken erkngken krjngkjg k getattr(mglkren
+                                                  gmkmge
+ngkng rmgm rjngoen jrnkng  ))
---
+++ main.py
@@ -0,0 +1,14 @@
+dhjhdhddhduhdfbn berhberbhrg hebgegjnegj hrgbhgbjf f  rgr
+rrgnergnog gngjknge
+gnegnikgnej egerjgb getattr(gnig
+                            rnojrgn
+gnugnijger
+ grgbiregnker getattr(eggenjeng
+                      gengjnergjner
+gnmjkeng
+enljnegjngek
+gmimgkog
+gjnenlee
+gekengq))                   knkl    not     j   nk      n           njn         nntplibnnl;
+
+        matchnjkfn  ef k kje
